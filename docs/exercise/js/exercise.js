f05acdd05f2ae6bb0fb22017f55bcaa3a769c41d
sbslayos <german.martinez.mt@usa.edu.co>
2023-01-17 14:13:26 -0500

    exercise1

--- docs/exercise/js/exercise.js
+++ docs/exercise/js/exercise.js
@@ -1,9 +1,9 @@
 'use strict'
-function saludar() {
-    const nombre = prompt('Escribe tu nombre');
-    if (nombre === null) {
-      alert(':( No escribiste tu nombre.');
-    } else {
-      alert('Hola, ' + nombre);
-    }
-  }
+let form = document.getElementById('form');
+let title = document.getElementById('title');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    title.innerHTML = 'Hola!! ${form.name.value}!!';
+
+});
